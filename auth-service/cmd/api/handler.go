694b3873f7ad3e04729b4fb21618ade3a16ab5c9
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-06-21 15:49:57 +0700

    Change to StatusAccepted

--- auth-service/cmd/api/handler.go
+++ auth-service/cmd/api/handler.go
@@ -2,46 +2,40 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 )
 
 func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
-
 	var requestPayload struct {
 		Email    string `json:"email"`
 		Password string `json:"password"`
 	}
 
 	err := app.readJSON(w, r, &requestPayload)
-
 	if err != nil {
 		app.errorJSON(w, err, http.StatusBadRequest)
 		return
 	}
 
-	// validate the user agains the database
-
+	// validate the user against the database
 	user, err := app.Models.User.GetByEmail(requestPayload.Email)
-
 	if err != nil {
-		app.errorJSON(w, errors.New("invalid email or password"), http.StatusBadRequest)
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
-	// validate the password
 	valid, err := user.PasswordMatches(requestPayload.Password)
-
 	if err != nil || !valid {
-		app.errorJSON(w, errors.New("invalid email or password"), http.StatusBadRequest)
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
 	payload := jsonResponse{
 		Error:   false,
-		Message: "Authentication successful",
+		Message: fmt.Sprintf("Logged in user %s", user.Email),
 		Data:    user,
 	}
 
-	app.writeJSON(w, http.StatusOK, payload)
-
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
